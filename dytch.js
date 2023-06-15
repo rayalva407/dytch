fea64b8237b3af01885ee774ff67eff1405b2f34
rayalva407 <rayalva407@gmail.com>
2023-06-14 20:44:02 -0600

    removes dytch.js and moves code to the main.js file

--- dytch.js
+++
@@ -1,8 +0,0 @@
-function changeTitle(newTitle) {
-  currentTitle = document.querySelector("title");
-  currentTitle.innerText = newTitle;
-}
-
-let change = prompt("Please enter new tab name");
-
-changeTitle(change);
